0fbd18d605dc83575f618b670854931fb3be1d93
veyseltosun <postalarim1923@gmail.com>
2022-12-15 03:29:48 +0300

    main structur is done.

---
+++ router/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Card from './components/Card';
+import AppRouter from './router/AppRouter';
+
+
+function App() {
+  return (
+    <div className="App">
+     
+      <Card/>
+      <AppRouter/>
+ 
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ router/src/router/AppRouter.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+import About from '../pages/About';
+import Css from '../pages/Css';
+import Home from '../pages/Home';
+import Html from '../pages/Html';
+import Logo from '../pages/Logo';
+import Services from '../pages/Services';
+
+
+function AppRouter() {
+  return (
+    <Router>
+        <Navbar/>
+        <Routes>
+            <Route path='/services' element={<Services/>} />
+            <Route path='/about' element={<About/>} />
+            <Route path='/css' element={<Css/>} />
+            <Route path='/html' element={<Html/>} />
+            <Route path='/logo' element={<Logo/>} />
+            <Route path='/' element={<Home/>} />
+        </Routes>
+        <Footer/>
+    </Router>
+  )
+}
+
+export default AppRouter
